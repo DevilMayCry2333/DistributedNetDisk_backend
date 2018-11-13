bb4051ec284ba7a30e30bf5f2b8958ad1e32e294
DevilMayCry2333 <1712363499@qq.com>
2018-11-13 18:20:17 +0800

    登录框样式

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	$("#login").click(function(){
+		//alert("Hello Jquery!");
+		var username=$("#username").val();
+		var password=$("#password").val();
+		if(username==""){
+			$("#br1").replaceWith("<p id='p1'>Username is null!</p>");
+		}
+		else{
+			$("#p1").replaceWith("<br id='br1'>");
+		}
+		if(password==""){
+			$("#br2").replaceWith("<p id='p2'>Password is null!</p>");
+		}
+		else{
+			$("#p2").replaceWith("<br id='br2'>");
+		}
+		if(username!="" && password!=""){
+			$.ajax({
+				type:"POST",
+				url:"#####",
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				data:{
+					"username":username,
+					"password":password
+				},
+			});
+		}
+	});
+});
