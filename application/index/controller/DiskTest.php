d0aa376c2ece0bd54e3483936e3f2eadd7ea3273
SV <svjiang@foxmail.com>
2018-11-25 01:37:34 +0800

    前端整合

---
+++ application/index/controller/DiskTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SV
+ * Date: 2018/11/25
+ * Time: 1:30
+ */
+
+namespace app\index\controller;
+
+
+use think\Controller;
+
+header("Content-Type: text/html;charset=utf-8");
+class DiskTest extends Controller
+{
+    public function show(){
+        header("Content-Type: text/html;charset=utf-8");
+
+    }
+}
