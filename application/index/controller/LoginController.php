c6c44b75bd7e063d54c92d4ee1dda93fc3ac2aae
DevilMayCry2333 <1712363499@qq.com>
2018-11-24 22:18:24 +0800

    和master合并

---
+++ application/index/controller/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: youkaiyu
+ * Date: 2018/11/23
+ * Time: 20:36
+ */
+
+namespace app\index\controller;
+use think\Controller;
+header("Access-Control-Allow-Origin: * ");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie,token');
+header("Content-Type: text/html;charset=utf-8");
+
+/**
+ * 浏览器第一次在处理复杂请求的时候会先发起OPTIONS请求。路由在处理请求的时候会导致PUT请求失败。
+ * 在检测到option请求的时候就停止继续执行
+ */
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+    exit;
+}
+
+
+class LoginController extends Controller
+{
+
+
+    public function login(){
+        header("Content-Type: text/html;charset=utf-8");
+        $username = $_GET["username"];
+        $password = $_GET["password"];
+        if($username!=null && $password!=null){
+
+
+            $user_res = new \app\index\model\LoginModller();
+            $user_res->login($username,$password);
+
+        }
+
+    }
+
+}
---
+++ application/index/model/LoginModller.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: youkaiyu
+ * Date: 2018/11/24
+ * Time: 01:06
+ */
+
+namespace app\index\model;
+
+
+use app\index\model\util\PasswordEncryt;
+use think\Model;
+header("Content-Type: text/html;charset=utf-8");
+class LoginModller extends Model
+{
+    protected $table = "netdisk_user";
+
+    public function login($username,$password){
+        header("Content-Type: text/html;charset=utf-8");
+        $passwordEncrypt = new PasswordEncryt();
+
+
+        $passmd5 = $passwordEncrypt->md5encrypt($username,$password);
+
+        $login_res = $passwordEncrypt->where('username',$username)->where('password',$passmd5)->find();
+
+        if($login_res!=null){
+            echo "OK";
+        }else{
+            echo "FAIL";
+        }
+    }
+
+}
